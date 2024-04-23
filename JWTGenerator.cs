e940f18f790a80fc8e87f68307d9955bc35abf83
nguyenvanbinhx2 <48704077+nguyenvanbinhx2@users.noreply.github.com>
2024-04-23 17:33:30 +0700

    Update JWTGenerator.cs

--- JWTGenerator.cs
+++ JWTGenerator.cs
@@ -10,7 +10,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.IdentityModel.Tokens;
 
-namespace Library
+namespace TestConjurAuthnAPIKey
 {
     public class JWTGenerator
     {
